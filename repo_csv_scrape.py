fb0072d35e799e2ab2a30f7bbd9ee6ae74dfc9b8
22f3001377 <22f3001377@ds.study.iitm.ac.in>
2024-10-30 21:15:04 +0530

    Create repo_csv_scrape.py
    This is the Python code to scrape repositories of github users with the following data:
    
    repositories.csv has these users' public repositories. For each user in users.csv, fetch up to the 500 most recently pushed repositories, with fields:
    
        login: The Github user ID (login) of the owner, which, BTW, is not directly in the API response.)
        full_name: Full name of the repository
        created_at: When the repository was created
        stargazers_count: Number of stars the repository has
        watchers_count: Number of watchers the repository has
        language: The programming language the repository is written in
        has_projects: Whether the repository has projects enabled
        has_wiki: Whether the repository has a wiki
        license_name: Name of the license the repository is under (This is under license.key)

---
+++ repo_csv_scrape.py
@@ -0,0 +1,62 @@
+import requests
+import csv
+GITHUB_TOKEN = '<My API token'
+headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+
+def get_repositories(username):
+    url = f'https://api.github.com/users/{username}/repos?per_page=100&page='
+    repositories = []
+    
+    page = 1
+    while True:
+        response = requests.get(url + str(page), headers=headers)
+        response.raise_for_status()
+        repos = response.json()
+        
+        if not repos:   
+            break
+        
+        for repo in repos:
+            repositories.append({
+                'login': username,
+                'full_name': repo.get('full_name', ''),
+                'created_at': repo.get('created_at', ''),
+                'stargazers_count': repo.get('stargazers_count', 0),
+                'watchers_count': repo.get('watchers_count', 0),
+                'language': repo.get('language', ''),
+                'has_projects': repo.get('has_projects', False),
+                'has_wiki': repo.get('has_wiki', False),
+                'license_name': repo['license']['name'] if repo.get('license') else ''   Updated line
+            })
+        
+        page += 1
+        if len(repositories) >= 500:
+            break
+    
+    return repositories[:500]
+
+def main():
+     Read users from users.csv
+    users = []
+    with open('users.csv', mode='r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            users.append(row['login'])   Collect GitHub usernames
+    with open('repositories.csv', mode='w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = [
+            'login', 'full_name', 'created_at', 'stargazers_count', 
+            'watchers_count', 'language', 'has_projects', 
+            'has_wiki', 'license_name'
+        ]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for username in users:
+            print(f'Fetching repositories for user: {username}')
+            repos = get_repositories(username)
+            for repo in repos:
+                writer.writerow(repo)
+
+if __name__ == '__main__':
+    main()
